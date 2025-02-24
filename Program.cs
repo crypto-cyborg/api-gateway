4a6949bb47d8194b5d9e186f437b6d8ca22a61d7
tng <m.farm4nov@gmail.com>
2025-02-24 15:56:01 +0400

    simple ocelot api gateway

---
+++ Program.cs
@@ -0,0 +1,24 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Configuration.AddJsonFile("ocelot.json");
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseOcelot().Wait();
+
+// app.UseHttpsRedirection();
+
+app.Run();
